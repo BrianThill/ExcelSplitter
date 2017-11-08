78b66270ddae3e85d7d16d42824996a78957e683
Brian Thill <brian.thill@elliemae.com>
2017-11-08 15:47:54 -0800

    New command exe to split excel into separate csv files

--- ExcelSplitter/Module1.vb
+++ ExcelSplitter/Module1.vb
@@ -1,27 +1,57 @@
 ﻿Imports Microsoft.VisualBasic.FileIO
 Imports Microsoft.Office.Interop
 Imports Microsoft.Office.Interop.Excel.XlFileFormat
-
+Imports System.IO
 
 Module Module1
 
-    Sub Main()
-        Dim cnt As Integer
-        Dim fname As String
+    Sub Main(args As String())
+
+        Dim fname, path, full_fname As String
         Dim wbSource, wbTarget As Excel.Workbook
         Dim ws As Excel.Worksheet
         Dim App As New Excel.Application
-        fname = "c:\temp\test.xlsx"
-        If System.IO.File.Exists(fname) Then
-            wbSource = App.Workbooks.Open(fname)
-            If wbSource IsNot Nothing Then
-                For Each ws In wbSource.Worksheets
-                    fname = "c:\temp\test" & CStr(cnt) & ".csv"
-                    wbTarget = App.Workbooks.Add()
-                    ws.Copy(wbTarget)
-                    wbTarget.SaveAs(fname, xlCSV)
-                Next
-            End If
+        If args.Count = 1 Then
+            fname = args(0)
+        Else
+            Console.WriteLine("Input filename not provided")
+        End If
+
+        path = Directory.GetCurrentDirectory
+        full_fname = System.IO.Path.GetFullPath(fname)
+
+        If Not System.IO.File.Exists(full_fname) Then
+            full_fname = ""
+            Console.WriteLine("Input file not found: " & fname)
+        End If
+
+
+        If full_fname <> "" Then
+
+            Try
+                wbSource = App.Workbooks.Open(full_fname)
+                If wbSource IsNot Nothing Then
+                    For Each ws In wbSource.Worksheets
+                        fname = path & "\" & ws.Name & ".csv"
+                        wbTarget = App.Workbooks.Add()
+                        ws.Copy(After:=wbTarget.Worksheets(1))
+                        wbTarget.SaveAs(fname, xlCSV)
+                    Next
+                End If
+
+
+
+            Catch ex As Exception
+            Finally
+                If wbSource IsNot Nothing Then
+                    wbSource.Saved = True
+                    wbSource.Close(SaveChanges:=False)
+                End If
+                App.Quit()
+                Environment.Exit(0)
+            End Try
+
+
         End If
 
     End Sub
